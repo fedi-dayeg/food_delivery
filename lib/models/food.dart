468d0f50ff270eaadeba333e3ec59b016074ff98
fedi <fedi.dayeg@hotmail.com>
2020-10-09 02:23:34 +0100

    add the HomePage, the Scaffold and Search TextField

---
+++ lib/models/food.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class Food {
+  final String imageUrl;
+  final String name;
+  final double price;
+
+  Food({@required this.imageUrl, @required this.name, @required this.price});
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/data/data.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Food Delivery'),
+        leading: IconButton(
+          icon: Icon(Icons.account_circle),
+          iconSize: 30.0,
+          onPressed: () {},
+        ),
+        actions: [
+          FlatButton(
+            onPressed: () {},
+            child: Text(
+              'Cart (${currentUser.cart.length})',
+              style: TextStyle(color: Colors.white, fontSize: 15.0),
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(width: 0.8),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(
+                      width: 0.8, color: Theme.of(context).primaryColor),
+                ),
+                hintText: 'Search Food or Restaurants',
+                prefixIcon: Icon(
+                  Icons.search,
+                  size: 30.0,
+                  color: Colors.deepOrangeAccent,
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    Icons.clear,
+                    size: 30.0,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
