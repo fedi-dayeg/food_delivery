f18493ebbfbe85c688d5bb7494a70fd5fa53aa11
fedi <fedi.dayeg@hotmail.com>
2020-10-10 01:05:52 +0100

    Add the restaurant description and the 2 button

--- lib/screen/restaurant_screen.dart
+++ lib/screen/restaurant_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/models/restaurant.dart';
+import 'package:food_delivery/widgets/rating_stars.dart';
 
 class RestaurantScreen extends StatefulWidget {
   final Restaurant restaurant;
+
   RestaurantScreen({this.restaurant});
+
   @override
   _RestaurantScreenState createState() => _RestaurantScreenState();
 }
@@ -45,7 +48,68 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
                     ),
                   ],
                 ),
-              )
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.restaurant.name,
+                      style: TextStyle(
+                          fontSize: 22.0, fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      '0.2 miles away',
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                  ],
+                ),
+                RatingStars(
+                  rating: widget.restaurant.rating,
+                ),
+                SizedBox(
+                  height: 6.0,
+                ),
+                Text(
+                  widget.restaurant.address,
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FlatButton(
+                onPressed: () {},
+                child: Text(
+                  'Reviews',
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                ),
+                color: Theme.of(context).primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Text(
+                  'Contact',
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                ),
+                color: Theme.of(context).primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+              ),
             ],
           )
         ],
