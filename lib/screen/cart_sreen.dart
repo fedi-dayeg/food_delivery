70477b392c223501f956ab37fc90e2f70cbe023c
fedi <fedi.dayeg@hotmail.com>
2020-10-12 00:31:03 +0100

    Finish the Cart Screen TODO: minimize the image width that depend on the phone screen

--- lib/screen/cart_sreen.dart
+++ lib/screen/cart_sreen.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/data/data.dart';
+import 'package:food_delivery/models/order.dart';
 
 class CartScreen extends StatefulWidget {
   @override
@@ -7,6 +9,111 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
+  _buildCaartItem(Order order) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      height: 170.0,
+      child: Row(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Container(
+                  width: 150.0,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(order.food.imageUrl),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(15.0)),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          order.food.name,
+                          style: TextStyle(
+                              fontSize: 20.0, fontWeight: FontWeight.bold),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Text(
+                          order.restaurant.name,
+                          style: TextStyle(
+                              fontSize: 16.0, fontWeight: FontWeight.w600),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Container(
+                          width: 100.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            border:
+                                Border.all(width: 0.8, color: Colors.black54),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              GestureDetector(
+                                child: Text(
+                                  '-',
+                                  style: TextStyle(
+                                      color: Theme.of(context).primaryColor,
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                onTap: () {},
+                              ),
+                              SizedBox(
+                                width: 20.0,
+                              ),
+                              Text(
+                                order.quantity.toString(),
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                              SizedBox(
+                                width: 20.0,
+                              ),
+                              GestureDetector(
+                                child: Text('+',
+                                    style: TextStyle(
+                                        color: Theme.of(context).primaryColor,
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.w600)),
+                                onTap: () {},
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              '\$${order.quantity * order.food.price}',
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,10 +122,8 @@ class _CartScreenState extends State<CartScreen> {
       ),
       body: ListView.separated(
           itemBuilder: (BuildContext context, int index) {
-            return Container(
-              height: 100.0,
-              color: Colors.red,
-            );
+            Order order = currentUser.cart[index];
+            return _buildCaartItem(order);
           },
           separatorBuilder: (BuildContext context, int index) {
             return Divider(
